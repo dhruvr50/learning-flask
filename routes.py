9bc9b9a7c5ce28ac27a84436d242519c18454fc9
Dhruv H. Raithatha <dhruvr50@gmail.com>
2017-03-13 22:26:45 -0400

    Created About Page & added to Home Page

--- routes.py
+++ routes.py
@@ -10,6 +10,9 @@ app = Flask(__name__)
 def index():
 	return render_template("index.html")
 
+@app.route("/about")
+def about():
+	return render_template("about.html")
 
 if __name__ == "__main__":
 	#Run the app on a local server
